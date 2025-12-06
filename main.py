8bff71be416af2ba2615e8d82d377c3eb3c85d16
alphatee <alphatee.herrera@gmail.com>
2025-12-05 20:49:27 -0800

    last commit, added pydantic, evaluated openapi.json file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException
-
+from pydantic import BaseModel
 app = FastAPI()
+
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
 
 items = []
 
@@ -15,16 +19,16 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
 
-@app.get("/items")
+@app.get("/items", response_model=list[Item])
 def list_items(limit: int = 10):
     return items[0:limit]
 
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
